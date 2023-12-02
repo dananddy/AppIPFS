07eedb4d71ca00095583312b4743b55af9f3a357
Bohdan <dan.anddy>
2023-12-02 23:34:04 +0200

    Initial Commit

---
+++ AppIPFS/AppIPFSApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppIPFSApp.swift
+//  AppIPFS
+//
+//  Created by busido on 02.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct AppIPFSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
